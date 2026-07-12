56ce53e11fc027ec40c81252c3f21523bff643a5
Shresth-11 <jaisshresth143@gmail.com>
2026-07-12 10:24:48 +0530

    Configure production knex connectionString for DATABASE_URL

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -25,14 +25,19 @@ const config: { [key: string]: Knex.Config } = {
   },
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || "5432"),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL
+      ? {
+          connectionString: process.env.DATABASE_URL,
+          ssl: { rejectUnauthorized: false },
+        }
+      : {
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || "5432"),
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          ssl: { rejectUnauthorized: false },
+        },
     migrations: {
       directory: path.join(__dirname, "src/db/migrations"),
       extension: "ts",
