3b646c12735d8e16c01764ed87eefdb9824a43be
Shresth-11 <jaisshresth143@gmail.com>
2026-07-12 10:18:58 +0530

    Configure ESM CommonJS Node16 TS compiler interoperability

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,9 +1,10 @@
 import knex from "knex";
 import path from "path";
 const config = require("../../knexfile");
+const knexConfig = config.default || config;
 
 const environment = process.env.NODE_ENV || "development";
-const db = knex(config[environment]);
+const db = knex(knexConfig[environment]);
 
 export default db;
 export { db };
